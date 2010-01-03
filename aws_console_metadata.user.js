17ec1f2dd46e06a5b2f17459a812ab9fd70decd7
unknown <jschoch@engineyard.com>
2010-01-02 19:47:23 -0500

    implemented name change in table, moves id to id in span

--- aws_console_metadata.user.js
+++ aws_console_metadata.user.js
@@ -22,6 +22,8 @@ var mhash = new Object();
 var timeout = 1000;
 var ajaxQueue = [];
 var requests = 0;
+
+// this gets us our XSS
 var processAjaxQueue = function(){
   if (ajaxQueue.length > 0) {
     for (ajax in ajaxQueue) {
@@ -67,51 +69,68 @@ function loadCSS()
                     	}
 		});
 	}
-
-// main jQuery funciton *** alias for document.ready
-(function() {
- 	
- 	// insert css we will use for our tool tip stuff
-	loadCSS();
-	
-	$('#top_nav').append("<span id=activate_aws_hack style='background-color: #FFFF00' > Waiting for Instance table to be loaded... </span> ")
-	// todo add loading image here
-
- 	// Listener for instances table
-	var interval;
-	$originalContent = $('#instances_datatable_hook').text();
-	interval = setInterval(function()
+function change_id_to_name()
+	{
+	$("td.yui-dt8-col-instanceId div span").each(function()
 		{
-		console.log("testing");
-    		if($originalContent != $('#instances_datatable_hook').text()) 
-			{
-			console.log("Content Changed");
-               		$originalContent = $('#instances_datatable_hook').text();
-			getMeta();
-			clearInterval(interval);
-               		}
-       		},500);	
-	// setup for test page /jqtest/table.html
-	$('#test').click(function ()
+		var $cell = $(this);
+		var id = $cell.text();
+		id = jQuery.trim(id);
+		console.log(id);
+		var name = "fun";
+		url = server_url;
+		url += "dev/name?aws_id=" + id + "&getName=1";
+		gmAjax({
+			url: url,
+			method: 'GET',
+			onload: function(response)
+				{
+				name = response.responseText;
+				if (name.search('error') == -1)
+					{
+					stopListen = true;
+					console.log("NAME: "+name);
+					// add name to table
+					//$cell.parent().parent().parent().append("<td class='yui-dt8-col-funName yui-dt-col-funName yui-dt-sortable yui-dt-resizeable' headers='yui-dt8-th-funname'><div class=yui-dt-liner>"+name+"</div></td>");
+					$cell.text(name);
+					$cell.attr("id",id);
+					console.log($cell.text());
+					$originalContent = $('#instances_datatable_hook').text();
+					stopListen = false;
+					}
+				},
+			onerror: function(response){
+                       		console.error('ERROR' + response.status );
+                   		}
+			});
+		});
+	}
+function getAWS_ID(obj)
+	{
+	var idattr = obj.attr("id");
+	if(idattr)
 		{
- 		$("th:first").text("INSTANCE ID");		    	
-    		});
-	}()); // end doc ready
-
+		return idattr;	
+		}
+	else
+		{
+		id = obj.text();
+		id = jQuery.trim(id);
+		return id;
+		}
+	}
 function getMeta ()
 	{
 	console.log("Setting up meta data:");
-	// listen for hover 
-	//$('#top_nav span#activate_aws_hack').click( function (c) {
+	change_id_to_name();
 	$('#top_nav span#activate_aws_hack').text("Meta Hack Activated"); 
-		
-	    $("td.yui-dt8-col-instanceId div span").click(function (c) {
-		// Set listener for <ctrl> + click so we can edit our meta data
+
+	// Set listener for <ctrl> + click so we can edit our meta data
+	$("td.yui-dt8-col-instanceId div span").click(function (c) {
 		if (c.ctrlKey)
 			{
 			//window.open(href, windowName, useParams);	
-			var id = $(this).text();
-			id = jQuery.trim(id);
+			var id = getAWS_ID($(this));
 			url = server_url;
 			url += "dev/edit_dev?aws_id=" + id;
 			console.log("Total URL Requests: "+requests);
@@ -130,8 +149,7 @@ function getMeta ()
 		var $trs = $(this).parent().parent().parent();
 		var dns = $trs.children('td.yui-dt8-col-dnsName').text();
 		// grab the AWS id in the current cell
-		var id = $(this).text();
-		id = jQuery.trim(id);
+		var id = getAWS_ID($(this));
 		// construct url for ajax	
 		var url = server_url;
 		url += '?aws_id='+id;
@@ -183,7 +201,7 @@ function getMeta ()
 	    console.log("Listiner should be in place");
 	    
 
-    }	
+    } // end getMeta
 
 function makeTT(e,id,dns,responseText)
 	{
@@ -191,10 +209,11 @@ function makeTT(e,id,dns,responseText)
 	var html = '<div id="info">';
 	html +=    '<b>Meta Data for: '+id+'</b><span id=close_tip>X</span>';
 	html +=	   '<p>'+ responseText +'</p>';
-	var clippyObject = clippy(dns);
-	html += clippyObject;
+	// don't put clippy object or browse link if there is no DNS	
 	if (dns != "")
 		{
+		var clippyObject = clippy(dns);
+		html += clippyObject;
 		html +=		'<a href=http://'+dns+'>Browse</a></div>';
 		}
 	console.log(responseText);
@@ -207,7 +226,7 @@ function makeTT(e,id,dns,responseText)
 		console.log("close tip clicked");
 		$('#info').remove();
 		});
-	}
+	} // end makeTT (make tooltip)
 
 function clippy(url)
 	{
@@ -233,5 +252,49 @@ function clippy(url)
 	'</object>'].join('');
 	
 	return clippy;
-	}
+	}// end clippy
+
+
+// main jQuery funciton *** alias for document.ready
+var stopListen = false;
+var $originalContent;
+(function() {
+	// insert css we will use for our tool tip stuff
+	loadCSS();
+	
+	$('#top_nav').append("<span id=activate_aws_hack style='background-color: #FFFF00' > Waiting for Instance table to be loaded... </span> ")
+	// todo add loading image here
+
+ 	// Listener for instance table changes
+	var interval;
+	$originalContent = $('#instances_datatable_hook').text();
+	interval = setInterval(function()
+		{
+		// if we are changing content ignore the changes
+		if (stopListen)
+			{
+			//$originalContent = $('#instances_datatable_hook').text();
+			}
+		else
+			{
+    			if($originalContent != $('#instances_datatable_hook').text()) 
+				{
+				console.log("Content Changed");
+               			$originalContent = $('#instances_datatable_hook').text();
+				// set timeout so table can load, 
+				// TODO: may want to make this user settable
+				setTimeout(getMeta,500);
+				//clearInterval(interval);
+               			}
+			}
+       		},500);	
+	// setup for test page /jqtest/table.html
+	$('#test').click(function ()
+		{
+ 		$("th:first").text("INSTANCE ID");		    	
+    		});
+	}()); // end doc ready
+
+	
+
 
